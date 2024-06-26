bd50d9811efee7821a5259da112c2432fb732e41
abhijeet <102318826+abhijeet5ire@users.noreply.github.com>
2024-06-26 11:39:12 +0530

    Update Question1.js

--- src/components/Question1.js
+++ src/components/Question1.js
@@ -9,7 +9,7 @@ const Question1 = () => {
 
   return (
       <>
-        <QuestionBox question={question} setQuestion={setQuestion} />       
+        <QuestionBox question={questions.[0].question} setQuestion={setQuestion} />       
       </>
   );
 };
